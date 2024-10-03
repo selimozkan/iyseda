394d92c0d564e2776e7c47ca8168684455b9a59c
Selim Ozkan <selimozkan@gmail.com>
2024-10-04 00:03:51 +0300

    Static settings

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.HomePage, name="home"),
+    path("change-languge/<str:lng>", views.ChangeLanguage, name="change-language"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 def HomePage(request):
     return render(request, "index.html")
+
+
+def ChangeLanguage(request, lng="en"):
+    request.session["language"] = "%s" % lng
+    if request.META.get("HTTP_REFERER"):
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+    else:
+        return HttpResponseRedirect("/")
